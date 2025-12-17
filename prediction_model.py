1e89566a8a305381edb28f1186b8c1d886f3450a
aluelmabior468 <aluelmabior@gmail.com>
2025-12-17 18:24:39 +0300

    importing python libraries

--- prediction_model.py
+++ prediction_model.py
@@ -1,1 +1,13 @@
+#Importing python libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from google.colab import files
+uploaded = files.upload()
 
+import pandas as pd
+student_data = pd.read_csv("student_performance.csv")
+from google.colab import drive
+drive.mount('/content/drive')
